1d49e3365f9f3a6de5af92b866f4df6c10248d55
Christos <chris.paraskeva10@gmail.com>
2017-03-07 18:57:45 +0000

    adding a start page and buttons to navigate / need to fix test

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -5,11 +5,15 @@ class BookmarkManager < Sinatra::Base
   #enable :sessions
 
   get "/" do
+    erb(:index)
+  end
+
+  get "/bookmarks" do
     links = []
     Link.all.each do |l|
       links << l.url.to_s
     end
-    @links = links.join(' ')
+    @links = links.join("\n")
     erb(:database)
   end
 end

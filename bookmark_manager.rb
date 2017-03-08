739600d68c4ead908120f552ab779e125e0e8970
Christos <chris.paraskeva10@gmail.com>
2017-03-08 11:04:56 +0000

    fixed failing test for checking links on bookmarks page

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -2,18 +2,13 @@ require 'sinatra/base'
 require './app/models/link'
 
 class BookmarkManager < Sinatra::Base
-  #enable :sessions
 
   get "/" do
     erb(:index)
   end
 
   get "/bookmarks" do
-    links = []
-    Link.all.each do |l|
-      links << l.url.to_s
-    end
-    @links = links.join("\n")
+    @link = Link.all
     erb(:database)
   end
 end
--- spec/view_links_spec.rb
+++ spec/view_links_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.feature 'it will display the links' do
   scenario 'so the user can see what has been added' do
     visit "/"
-    click_button('View Bookmarks')
+    click_link ('View Bookmarks')
     # Link.create(:id => 6, :title => 'eBay', :url => 'http//www.ebay.co.uk')
     expect(page).to have_content ('http://www.google.co.uk')
   end

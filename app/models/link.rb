02ead37db02113cac19ad68a4b19a339270bacf5
Christos <chris.paraskeva10@gmail.com>
2017-03-08 17:03:46 +0000

    init

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,4 @@
+DATABASE_URL = postgres://hfphdwnmtuoqim:af46470b6a78ec7eb21974a5c3daaf5b05b146fb903ed9dd0f69ebf5015856fb@ec2-54-235-123-159.compute-1.amazonaws.com:5432/d6eicuho6vtlg8
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
@@ -9,6 +10,6 @@ class Link
 end
 # the interpolation used for the 'ENV' settings will set the ending of this database name
 # to 'test', whereas when the app is used via rackup, it will use the development mode.
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
